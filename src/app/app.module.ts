de862b4054826f1743fa00e5df461842c54cbcc8
Mandy Buchhold <mandy.buchhold@capgemini.com>
2019-04-11 13:53:26 +0200

    11.04.19  JumpTheQueue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Application components and services
 import { AppComponent } from './app.component';
+import { CoreModule } from './shared/core.module';
+import { AppRoutingModule } from './app-routing.module';
+import { FormLoginModule } from './form-login/form-login-module';
+import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { ViewQueueComponent } from './view-queue/view-queue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HeaderComponent,
+    ViewQueueComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    FormLoginModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrlRestServices: 'http://localhost:8081/mythaistar/services/rest'
 };
 
 /*
